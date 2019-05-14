016693183d27c9ec376085054280abbdc8a2fe37
soulmatewhat <soul_mate_1@sina.com>
2019-05-14 10:16:45 +0800

    图标,主页,模块创建

--- news/info/modules/index/views.py
+++ news/info/modules/index/views.py
@@ -1,7 +1,10 @@
 from . import index_blue
+from flask import render_template, current_app
 
 
-
-@index_blue.route('/index')
+@index_blue.route('/')
 def index():
-    return 'index'
+    return render_template('news/index1.html')
+@index_blue.route('/favicon.ico')
+def favicon():
+    return current_app.send_static_file('news/favicon.ico')
